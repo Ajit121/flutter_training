111aaff9fa9389127e35b8388ad350c5e696615b
ajitjaiswal <Ajit1234>
2020-04-22 20:13:07 +0530

    Inital commit

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+final String BASE_URL = "http://dev.maxmobility.in:81/grabpos-web/dev/public/";
+//final String LOGIN_URL = BASE_URL+"api/v2/user/login";
+final String LOGIN_URL = "http://billing.thehazelnutfactory.com/api/v2/user/login";
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_training/constants.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -15,7 +19,14 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: HomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("App bar"),
+        ),
+        body: HomePage(
+
+        ),
+      ),
     );
   }
 }
@@ -27,60 +38,123 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int count = 0;
+  TextEditingController _usernameController, _passwordController;
+  final _formKey = GlobalKey<FormState>();
+  bool showLoader = false;
+  @override
+  void initState() {
+    _usernameController = TextEditingController();
+    _passwordController = TextEditingController();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("App bar"),
-      ),
-      body: Container(
+    return  Container(
         padding: const EdgeInsets.only(left: 20, right: 20),
         width: double.infinity,
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(60),
-              child: Image.asset('images/logo.png'),
-            ),
-            SizedBox(
-              height: 90,
-              child: TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), labelText: 'Username'),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(60),
+                child: Image.asset('images/logo.png'),
               ),
-            ),
-            SizedBox(
-              height: 90,
-              child: TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), labelText: 'Password'),
+              SizedBox(
+                height: 90,
+                child: TextFormField(
+                  controller: _usernameController,
+                  validator: (text) {
+                    if (text.isEmpty) {
+                      return "Please enter username";
+                    }
+                  },
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Username'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                  onPressed: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 40, vertical: 15),
-                    child: Text(
-                      'SUMBIT',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
+              SizedBox(
+                height: 90,
+                child: TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  validator: (text) {
+                    if (text.isEmpty) {
+                      return "Please enter password";
+                    }
+                  },
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Password'),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                 showLoader? CircularProgressIndicator() : RaisedButton(
+                    onPressed: () {
+
+                      bool isValid = _formKey.currentState.validate();
+                      if (isValid) {
+                        String name = _usernameController.text.trim();
+                        String password = _passwordController.text.trim();
+                        print('name: $name and password: $password');
+                        _login(name, password);
+                      }
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 15),
+                      child: Text(
+                        'SUMBIT',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            )
-          ],
+                ],
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  Future _login(String name, String password) async {
+    setState(() {
+      showLoader = true;
+    });
+    Map<String, dynamic> _body = Map();
+    _body.putIfAbsent("username", () => name);
+    _body.putIfAbsent("password", () => password);
+    var apiResponse = await http.post(LOGIN_URL,
+        body: {"username": "8240280249", "password": "123456"});
+    setState(() {
+      showLoader = false;
+    });
+    try {
+      print('response ${apiResponse.statusCode}');
+      print('response body ${json.decode(apiResponse.body).toString()}');
+      if (apiResponse.statusCode == 200) {
+        print('response body ${apiResponse.body}');
+        //  Map<String,dynamic>responseObj = json.decode(apiResponse.body);
+
+      }
+      if (apiResponse.statusCode == 500) {
+        print("sever error");
+      }
+    } catch (ex) {
+      print('exception happened $ex');
+    }
   }
 }
